d1b00d1ebdadf515ceeaf2dd430b4b1c7a8bb706
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-04-28 11:52:00 +0500

    add changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import Signup from "./components/Auth/Signup.jsx"
 import Board from './components/Dashboard/Board.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
